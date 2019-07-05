ca85e3f99d6bb1c6418a025794ff1a1c058229aa
Luis Ascencio <luismiguelascencio@gmail.com>
2019-07-05 17:25:50 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
 
+function nestedTarget() {
+  return document.getElementById("nested").querySelector("div.target");
+}
+
+function deepestChild() {
+  return document.querySelector("div#grand-node div div div div");
+}
+
+function increaseRankBy(n) {
+  const list = document
+    .getElementById("app")
+    .querySelectorAll("ul.ranked-list li");
+
+  for (let index = 0; index < list.length; index++) {
+    list[index].innerHTML = parseInt(list[index].innerHTML) + n;
+  }
+}
